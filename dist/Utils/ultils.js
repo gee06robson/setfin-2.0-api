44f1680597ea416026b7fb8ab9e7d89a6edc0a42
gee06robson <robsom06@hotmail.com>
2022-02-05 17:49:42 -0300

    Configuração Date Create Document

--- dist/Utils/ultils.js
+++ dist/Utils/ultils.js
@@ -4,6 +4,7 @@ exports.HandleDate = void 0;
 var HandleDate = function (date) {
     var toBreak = date.split("/").reverse().join("/");
     toBreak = new Date(toBreak).toISOString();
+    console.log(new Date(), "data");
     return toBreak;
 };
 exports.HandleDate = HandleDate;
